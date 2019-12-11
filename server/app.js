1d9a3d6a6a64378d80c7aabd05c7a89841db4200
Taylormm13 <657467163@qq.com>
2019-12-11 17:58:38 +0800

    新增部分代码；

---
+++ server/app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const schema = require("./schema/schema");
+const mongoose = require("mongoose");
+
+const app = express();
+
+//* connect to mongodb database
+mongoose
+  .connect(
+    "mongodb+srv://Taylormm:520hanjiaren@cluster0-jigdq.mongodb.net/test?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    }
+  )
+  .then(() => {
+    console.log("Connected to MongoBD Atlas");
+  })
+  .catch(err => {
+    console.log("Error: ", err.message);
+  });
+
+// mongoose.connect(
+//   "mongodb+srv://Taylormm:520hanjiaren@cluster0-jigdq.mongodb.net/test?retryWrites=true&w=majority"
+// );
+// mongoose.connection.once("open", () => {
+//   console.log("Connected to database");
+// });
+
+// mongodb+srv://Taylormm:520hanjiaren@cluster0-jigdq.mongodb.net/test?retryWrites=true&w=majority
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true
+  })
+);
+
+app.listen(4000, () => {
+  console.log("now listening for requests on port 4000");
+});
